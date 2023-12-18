9ccc4a5d74a7079286ebc26f2449b5840c9fd22c
Rodrigo D <neonrok@pm.me>
2023-12-18 15:29:34 +0000

    mais coisinhas

--- ficha10/scripts.js
+++ ficha10/scripts.js
@@ -5,6 +5,15 @@ function Nclick(){
     return FDP;
 }
 
-function sec1(){
-    
-}
+setInterval(() => {
+    console.log("passaram 5 segundos")
+}, 5000);
+
+setInterval(() => {
+    const data = new Date()
+    const horas = data.getHours();
+    const min = data.getMinutes();
+    const sec = data.getSeconds();
+    const hms = [horas , min , sec].join(':')
+    console.log(hms)
+}, 2000);
