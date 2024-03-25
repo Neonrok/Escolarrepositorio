575a4345fd615999b771e821363a31b3db2b269b
Rodrigo D <neonrok@pm.me>
2024-03-25 22:10:08 +0000

    .

---
+++ semestre2/ficha4/ex2.js
@@ -0,0 +1,8 @@
+let pa = document.getElementById("p1");
+let pb = document.getElementsByTagName("img")[0];
+let pc = document.body
+
+
+pa.textContent="está a chover";
+pb.src="https://image.freepik.com/fotos-gratis/gota-de-chuva-caindo-no-chao-da-rua-em-um-dia-de-chuva-forte_1962-2005.jpg"
+pc.style.backgroundColor = "blue"
